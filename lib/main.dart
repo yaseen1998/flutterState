f73dc40dab11267244ab3036dfd07b3baa9e8893
yaseen1998 <yasseenkouthe@eng.hu.edu.jo>
2022-09-11 12:54:09 +0300

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,133 +12,70 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Color newcolor = Colors.white;
+  List image = ['mobile/11.jpeg', 'mobile/12.jpeg', 'mobile/13.jpeg', 'mobile/14 mini.jpeg','mobile/14 max.jpeg','mobile/11.jpeg', 'mobile/12.jpeg', 'mobile/13.jpeg', 'mobile/14 mini.jpeg','mobile/14 max.jpeg'];
+  List Name = ['Iphone 11', 'Iphone 12', 'Iphone 13', 'Iphone 14 mini','Iphone 14 max','Iphone 11', 'Iphone 12', 'Iphone 13', 'Iphone 14 mini','Iphone 14 max'];
+  List Price = ['1000', '2000', '3000', '4000','5000','1000', '2000', '3000', '4000','5000'];
+  List Colorss = [Colors.red, Colors.blue, Colors.green, Colors.yellow,Colors.pink,Colors.red, Colors.blue, Colors.green, Colors.yellow,Colors.pink];
+  List Iconss = [Icons.favorite, Icons.favorite, Icons.favorite, Icons.favorite,Icons.favorite,Icons.favorite, Icons.favorite, Icons.favorite, Icons.favorite,Icons.favorite];
+  List<bool> _selected = List.generate(10, (i) => false);
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
         body: Container(
-          color: newcolor,
-          child: ListView(
-            // parent listview
-            physics: BouncingScrollPhysics(), // this is for decoration
-            children: [
-              Column(
-                children: [
-                  const SizedBox(
-                    width: 100,
-                    height: 100,
-                    child: Image(
-                        image: NetworkImage(
-                            "https://th.bing.com/th/id/R.89ee28915ec60f85282cc057d363ef15?rik=MIaZ1cf%2fK8kYLg&pid=ImgRaw&r=0")),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(Icons.star),
-                      Icon(Icons.star_border),
-                      Icon(Icons.star_half),
-                    ],
-                  ),
-                  // insider list view (the child one )
-                ],
-              ),
+          // color: Colors.red,
+          child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  // padding: EdgeInsets.all(10),
+                  // color: Colors.red,
+                  child: ListTile(
+                    dense: true,
+                    // selected: index % 2 == 0 ? true : false,
+                    selectedColor: Colors.red,
+                    textColor: Color.fromARGB(255, 5, 84, 148),
+                    contentPadding: EdgeInsets.all(20),
+                    enabled: true,
+                    focusColor: Colors.yellow,
+                    mouseCursor: SystemMouseCursors.click,
+                    hoverColor: Colors.purple,
+                    enableFeedback: true,
+                    tileColor: _selected[index] ? Color.fromARGB(255, 106, 103, 103) : null,
+                    onTap: () =>
+                        setState(() => _selected[index] = !_selected[index]),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(
+                          color: Colorss[index],
+                          width: 2,
+                          strokeAlign: StrokeAlign.outside),
+                    ),
+                    visualDensity: VisualDensity(horizontal: 0, vertical: -1),
+                    isThreeLine: true,
+                    autofocus: false,
+                    subtitle: Text(
+                      'Price: \$${Price[index]}',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    leading: Icon(
+                      Iconss[index],
+                      color: Colorss[index],
+                      size: 30,
+                    ),
+                    trailing: Image(image:AssetImage(image[index])),
 
-              const Divider(
-                color: Colors.black,
-                thickness: 2,
-              ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    width: 100,
-                    height: 100,
-                    child: Image(
-                        image: NetworkImage(
-                            "https://th.bing.com/th/id/R.89ee28915ec60f85282cc057d363ef15?rik=MIaZ1cf%2fK8kYLg&pid=ImgRaw&r=0")),
+                    title: Text(Name[index],
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold)),
                   ),
-                  Column(
-                    children: const [
-                      Icon(Icons.stop),
-                      Icon(Icons.stop_circle),
-                      Icon(Icons.stop_circle_sharp),
-                    ],
-                  ),
-                ],
-              ),
-
-              const Divider(
-                color: Colors.black,
-                thickness: 2,
-              ),
-
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(Icons.stop_sharp),
-                      Icon(Icons.storage),
-                      Icon(Icons.store_mall_directory),
-                    ],
-                  ),
-                  const SizedBox(
-                    width: 100,
-                    height: 100,
-                    child: Image(
-                        image: NetworkImage(
-                            "https://th.bing.com/th/id/R.89ee28915ec60f85282cc057d363ef15?rik=MIaZ1cf%2fK8kYLg&pid=ImgRaw&r=0")),
-                  ),
-                ],
-              ),
-
-              const Divider(
-                color: Colors.black,
-                thickness: 2,
-              ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    children: const [
-                      Icon(Icons.shop),
-                      Icon(Icons.shop_two),
-                      Icon(Icons.share_arrival_time),
-                    ],
-                  ),
-                  const SizedBox(
-                    width: 100,
-                    height: 100,
-                    child: Image(
-                        image: NetworkImage(
-                            "https://th.bing.com/th/id/R.89ee28915ec60f85282cc057d363ef15?rik=MIaZ1cf%2fK8kYLg&pid=ImgRaw&r=0")),
-                  ),
-                ],
-              ),
-
-              const Divider(
-                color: Colors.black,
-                thickness: 2,
-              ),
-              // insider list view (the child one )
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(onPressed: () { setState(() { newcolor = Colors.red; }); }, color: Colors.red, icon: const Icon(Icons.circle)),
-                  IconButton(onPressed: () { setState(() { newcolor = Colors.blue; }); }, color: Colors.blue, icon: const Icon(Icons.circle)),
-                  IconButton(onPressed: () { setState(() { newcolor = Colors.orange; }); }, color: Colors.orange, icon: const Icon(Icons.circle)),
-                  IconButton(onPressed: () { setState(() { newcolor = Colors.green; }); }, color: Colors.green, icon: const Icon(Icons.circle)),
-                  IconButton(onPressed: () { setState(() { newcolor = Colors.white; }); }, color: Colors.white, icon: const Icon(Icons.circle)),
-
-                ],
-              ),
-            ],
-          ),
+                );
+              }),
         ),
       ),
     );
